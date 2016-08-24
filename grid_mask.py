e64781cdef7f9e4c40504a4aae7f98178bbd3f5a
crchanda98 <crchanda98@gmail.com>
2016-08-24 14:39:30 +0530

    Masking grided data from any parent grid
    This code is for a netcdf file. Can be used for others by changing the parent dataset

--- grid_mask.py
+++ grid_mask.py
@@ -2,7 +2,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 from netCDF4 import Dataset as dt
 
-dataset = dt('met_era_karnataka_2014_2015.nc', 'r')
+# LOAD DATA FROM NETCDF AS PARENT DATASET
+dataset = dt('your_netcdf_file.nc', 'r')
 lon = np.array(dataset['longitude'])
 lat = np.array(dataset['latitude'])
 zvar = np.array(dataset['skt'])
@@ -14,11 +15,12 @@ lon1 = 75
 lon2 = 76
 
 if lat1 > lat2 or lat1 < lat.min() or lat2 > lat.max():
-    print "INVALID DOMAIN"
+    print "INVALID DOMAIN: PLEASE"
 
 a1 = np.where(lat >= lat1)[0][-1]+1
 a2 = np.where(lat >= lat2)[0][-1]-1
-lat11 = lat[a2:a1+1]
+lat_new = lat[a2:a1+1]
 a1 = np.where(lon >= lon1)[0][0]-1
 a2 = np.where(lon >= lon2)[0][0]+1
-lon11 = lon[a1:a2+1]
+lon_new = lon[a1:a2+1]
+zvar_new = zvar[:, a2:a1+1, a1:a2+1]
